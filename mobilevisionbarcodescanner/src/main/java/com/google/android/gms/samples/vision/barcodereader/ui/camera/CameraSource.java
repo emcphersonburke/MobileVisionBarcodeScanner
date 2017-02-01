a090708a4cbe6741fdc899ba945bfd077c8192e8
emcphersonburke <e.mcpherson.burke@gmail.com>
2017-02-01 12:52:01 -0500

    Adding color inversion option for camera

--- mobilevisionbarcodescanner/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/CameraSource.java
+++ mobilevisionbarcodescanner/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/CameraSource.java
@@ -586,6 +586,22 @@ public class CameraSource {
             return false;
         }
     }
+    
+    public boolean setColorsInverted(boolean invert) {
+                synchronized (mCameraLock) {
+            if (mCamera != null && mode != null) {
+                Camera.Parameters parameters = mCamera.getParameters();
+                if (invert) {
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);   
+                } else {
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_NONE);   
+                }
+                mCamera.setParameters(parameters);
+            }
+
+            return false;
+        }
+    }
 
     /**
      * Starts camera auto-focus and registers a callback function to run when
