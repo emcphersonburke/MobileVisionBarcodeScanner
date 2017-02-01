168cee2953dc86ea4ff9ed0c1053fb27eee8cf1d
emcphersonburke <e.mcpherson.burke@gmail.com>
2017-02-01 13:21:17 -0500

    Bug fix

--- mobilevisionbarcodescanner/src/main/java/com/google/android/gms/samples/vision/barcodereader/BarcodeCapture.java
+++ mobilevisionbarcodescanner/src/main/java/com/google/android/gms/samples/vision/barcodereader/BarcodeCapture.java
@@ -249,9 +249,9 @@ public final class BarcodeCapture extends BarcodeFragment {
         mGraphicOverlay.setDrawRect(isShowDrawRect());
         mGraphicOverlay.setRectColors(getRectColors());
         mGraphicOverlay.setShowText(isShouldShowText());
+        mCameraSource.setColorsInverted(isShouldInvert());
         mCameraSource.setFocusMode(isAutoFocus() ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null);
         mCameraSource.setFlashMode(isShowFlash() ? Camera.Parameters.FLASH_MODE_TORCH : Camera.Parameters.FLASH_MODE_OFF);
-        mCameraSource.setColorsInverted(isShouldInvert());
     }
 
     /**
